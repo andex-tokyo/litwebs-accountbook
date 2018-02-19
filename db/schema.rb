703332ae737139557fed75ea26be5f06ef6fbcd9
Yuki Tsuchida <d@gahaku.tech>
2018-02-19 00:26:51 +0000

    カラムを足した

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219001731) do
+ActiveRecord::Schema.define(version: 20180219002546) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20180219001731) do
   create_table "items", force: :cascade do |t|
     t.string   "title"
     t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
